090c0c55954d0c35762706ba890ccfa1e222f5a0
aleksey-t <megapozitivizm@yandex.ru>
2024-01-14 16:15:56 +0300

    initial

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+const products = require("./sampleData.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/products", function (req, res, next) {
+  res.json({ products: products });
+});
+
+app.get("/products/:product_type", function (req, res, next) {
+  const productType = req.params.product_type;
+  let data = [];
+
+  if (productType) {
+    switch (productType) {
+      case "cakes":
+        data = products["cakes"];
+        break;
+      case "street_food":
+        data = products["street_food"];
+        break;
+      case "coffee_and_tea":
+        data = products["coffee_and_tea"];
+        break;
+    }
+  }
+
+  res.json({ products: data || products });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
